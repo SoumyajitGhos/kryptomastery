16617b8d5698ccf9effad3d40e5aae6c42e313eb
SoumyajitGhos <soumysjitghos@gmail.com>
2023-06-11 23:48:20 +0530

    basic structure of files and setup has been done for the frontend of the app with React.js and tailwindcss

---
+++ client/src/components/Navbar.jsx
@@ -0,0 +1,10 @@
+// import { HiMenuAlt4 } from 'react-icons/hi';
+// import { AiOutlineClose } from 'react-icons/ai';
+import logo from '../../images/logo.png';
+const Navbar = () => {
+    return (
+        <nav className="w-full flex md:justify-center"></nav>
+    );
+}
+
+export default Navbar;
