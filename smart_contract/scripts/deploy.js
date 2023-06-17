c531dcef3bba3eea481fa1417d9e8f25df8bbd8e
SoumyajitGhos <soumysjitghos@gmail.com>
2023-06-17 13:42:24 +0530

    Implementing the smart contract of the website

---
+++ smart_contract/scripts/deploy.js
@@ -0,0 +1,37 @@
+const main = async () => {
+  // const currentTimestampInSeconds = Math.round(Date.now() / 1000);
+  // const unlockTime = currentTimestampInSeconds + 60;
+
+  // const lockedAmount = hre.ethers.parseEther("0.001");
+
+  // const lock = await hre.ethers.deployContract("Lock", [unlockTime], {
+  //   value: lockedAmount,
+  // });
+
+  // await lock.waitForDeployment();
+
+  // console.log(
+  //   `Lock with ${ethers.formatEther(
+  //     lockedAmount
+  //   )}ETH and unlock timestamp ${unlockTime} deployed to ${lock.target}`
+  // );
+  const transactionsFactory = await hre.ethers.getContractFactory("Transactions");
+  const transactionsContract = await transactionsFactory.deploy();
+
+  await transactionsContract.deployed();
+
+  console.log("Transactions address: ", transactionsContract.address);
+}
+
+
+const runMain = async () => {
+  try {
+    await main()
+  } catch (error) {
+    ProcessingInstruction.exit(0);
+    console.error(error);
+    ProcessingInstruction.exit(1)
+  }
+}
+
+runMain();
